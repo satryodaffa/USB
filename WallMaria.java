e3a6b97162a5b47ff0a68ec96288ccadba9abffe
Muhammad Raditya Nayatama <92539088+Nayatamaa@users.noreply.github.com>
2022-06-21 22:08:35 +0800

    Update WallMaria.java

--- WallMaria.java
+++ WallMaria.java
@@ -2,15 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Project2_6;
+package FinalProject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 /**
  *
- * @author Satryo
+ * @author Nayatama
  */
+
 public class WallMaria {
-    public static void main(String[] args) {
+    public static void weakLayer() throws IOException {
         List<List<Integer>> wallMaria = new ArrayList<>();
         int maxIndex = 50;
         int weakestSpot = 0;
@@ -23,32 +30,49 @@ public class WallMaria {
         ArrayList<Integer> layer3 = new ArrayList<>();
         ArrayList<Integer> layer4 = new ArrayList<>();
         
-        // add layers to the array list
-        // layer 1
-        layer1.add(2);
-        layer1.add(4);
-        layer1.add(7);
-        wallMaria.add(layer1);
-        // layer 2
-        layer2.add(1);
-        layer2.add(5);
-        layer2.add(8);
-        layer2.add(10);
-        wallMaria.add(layer2);
-        // layer 3
-        layer3.add(3);
-        layer3.add(6);
-        layer3.add(7);
-        layer3.add(9);
-        wallMaria.add(layer3);
-        // layer 4
-        layer4.add(1);
-        layer4.add(7);
-        wallMaria.add(layer4);
+        BufferedReader bi = new BufferedReader(
+            new InputStreamReader(System.in));
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter number of layers: ");
+        int layers = sc.nextInt();
+        for(int i = 1; i-1 < layers; i++){
+            System.out.print("Enter brick edges of layer " + i + ": ");
+            String[] strNums = bi.readLine().split(" ");
+            for (int j = 0; j < strNums.length; j++) {
+                switch(i){
+                    case 1:
+                        layer1.add(Integer.parseInt(strNums[j]));
+                        break;
+                    case 2:
+                        layer2.add(Integer.parseInt(strNums[j]));
+                        break;
+                    case 3:
+                        layer3.add(Integer.parseInt(strNums[j]));
+                        break;
+                    case 4:
+                        layer4.add(Integer.parseInt(strNums[j]));
+                        break;
+                }
+            }
+            if(i == 1){
+                wallMaria.add(layer1);
+            }
+            else if(i == 2){
+                wallMaria.add(layer2);
+            }
+            else if(i == 3){
+                wallMaria.add(layer3);
+            }
+            else{
+                wallMaria.add(layer4);
+            } 
+        }
         
         // print the layer edges
         for(int wm = 0; wm < wallMaria.size(); wm++)
-        {   System.out.print("Layer " + (wm + 1) + " ");
+        {   System.out.print("Layer " + (wm + 1) + ": ");
         for(int l = 0; l < wallMaria.get(wm).size(); l++)
         {   System.out.print(" " + wallMaria.get(wm).get(l));
         totalSpots[wallMaria.get(wm).get(l)] = totalSpots[wallMaria.get(wm).get(l)] + 1;
@@ -65,7 +89,8 @@ public class WallMaria {
         }
         }
         System.out.println("");
-        System.out.println("Counter Weakest spot: " + weakestSpot);
-        System.out.println("Weakest spot: " + spotNumber);
-        }        
-    }
+//        System.out.println("Counter Weakest spot: " + weakestSpot);
+        System.out.println("Weakest part of the wall is at postion " + spotNumber);
+        }
+    
+}
