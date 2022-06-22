d9ec4a1d781707e266402fee2b58ab19f01c0404
MUHAMMAD AMIRUL HAQIMI BIN MOHD ZAMIR <92685367+amrlhqmii@users.noreply.github.com>
2022-06-22 19:05:49 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,6 +7,11 @@ package FinalProject;
 
 public class Main {
     public static void main(String[] args) {
+        
+        String filepath = "sasageyo.wav";
+        
+        Music musicObject = new Music();
+        musicObject.playMusic(filepath);
         LoginPage.runLoginPage();        
     }
 }
